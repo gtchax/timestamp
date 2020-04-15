fb7e76fc121c1ff753e91b1a269e5b0e46f3d1aa
Gwinyai Chakonda <gtchax@gmail.com>
2020-04-15 12:33:04 +0200

    changed handler function

--- server.js
+++ server.js
@@ -33,26 +33,44 @@ app.get('/api/timestamp', (req, res) => {
   res.json({"unix": date.getTime(), "utc" : date.toUTCString() })
 })
 
-app.get('/api/timestamp/:date_string', (req, res) => {
-  let param = req.params.date_string;
-  var date = new Date(param);
-  let checkDate = date.toUTCString()
+// app.get('/api/timestamp/:date_string', (req, res) => {
+//   let param = req.params.date_string;
+//   var date = new Date(param);
+//   let checkDate = date.toUTCString()
   
-  if(checkDate === 'Invalid Date') {
-    date = new Date(param*1000)
-    if(date.toUTCString() === 'Invalid Date') {
-      return res.json({"error": "Invalid Date" })
-    } else {
-      return res.json({"unix": date.getTime(), "utc" : date.toUTCString()})
-    }
+//   if(checkDate === 'Invalid Date') {
+//     date = new Date(param*1000)
+//     if(date.toUTCString() === 'Invalid Date') {
+//       return res.json({"error": "Invalid Date" })
+//     } else {
+//       return res.json({"unix": date.getTime(), "utc" : date.toUTCString()})
+//     }
     
-  }
-  res.json({"unix": date.getTime(), "utc": date.toUTCString()})
+//   }
+//   res.json({"unix": date.getTime(), "utc": date.toUTCString()})
    
     
   
   
-})
+// })
+
+app.get("/api/timestamp/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+
+  
+  if (/\d{5,}/.test(dateString)) {
+    var dateInt = parseInt(dateString);
+    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+  }
+
+  let dateObject = new Date(dateString);
+
+  if (dateObject.toString() === "Invalid Date") {
+    res.json({ error: "Invaid Date" });
+  } else {
+    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+  }
+});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
